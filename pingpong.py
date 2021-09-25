a47955bb4f071adc65f79395e6890b1d2f2f66aa
tomikhin <90508484+tomikhin@users.noreply.github.com>
2021-09-25 15:32:19 +0300

    Update pingpong.py

--- pingpong.py
+++ pingpong.py
@@ -12,19 +12,38 @@ def game():
     clock = time.Clock()
     FPS = 60
 
-    racket1 = Player('pixil-frame-0 (1).png', 30, 200, 4, 50, 150, win_width, win_height, window)
-    racket2 = Player('pixil-frame-0.png', 520, 200, 4, 50, 150, win_width, win_height, window)
-
-
+    racket1 = Player('pixil-frame-0 (1).png', 30, 200, 4, 20, 150, win_width, win_height, window)
+    racket2 = Player('pixil-frame-0.png', 520, 200, 4, 20, 150, win_width, win_height, window)
+    ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50, win_width, win_height, window)
+    font.init()
+    font1 = font.Font(None, 35)
+    lose1 = font1.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+    lose2 = font1.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+    speed_x = 3
+    speed_y = 3
     while run:
         for e in event.get():
             if e.type == QUIT:
                 run = False
-        window.fill(back)
-        racket1.reset()       
-        racket2.reset()
-        racket2.update_r()
-        racket1.update_l()
+        if finish != True:
+            window.fill(back)
+            racket1.reset()       
+            racket2.reset()
+            ball.reset()
+            if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+                speed_x *= -1
+            if ball.rect.y > win_height-50 or ball.rect.y < 0:
+                speed_y *= -1
+            ball.rect.x += speed_x
+            ball.rect.y += speed_y
+            racket2.update_r()
+            racket1.update_l()
+            if ball.rect.x < 0:
+                finish = True
+                window.blit(lose1, (200, 200))
+            if ball.rect.x > win_width:
+                finish = True
+                window.blit(lose2, (200, 200))
         display.update()
         clock.tick(FPS)
 
