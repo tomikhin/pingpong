308c854044f5bad04125d3a019a992b8d808a9d4
tomikhin <90508484+tomikhin@users.noreply.github.com>
2021-10-06 20:34:30 +0300

    Update pingpong.py

--- pingpong.py
+++ pingpong.py
@@ -1,32 +1,38 @@
 from pygame import*
+#import time
+
 from GameSprite import GameSprite
 from Player import Player
 def game():
-    back = (200, 255, 255)
+    back = "background.png"
+    #start_time = time.clock()
     win_width = 600
     win_height = 500
     window = display.set_mode((win_width, win_height))
-    window.fill(back)
+    
     run = True
     finish = False
     clock = time.Clock()
     FPS = 60
+    background = transform.scale(image.load(back), (win_width, win_height))
 
     racket1 = Player('pixil-frame-0 (1).png', 30, 200, 4, 20, 150, win_width, win_height, window)
     racket2 = Player('pixil-frame-0.png', 520, 200, 4, 20, 150, win_width, win_height, window)
     ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50, win_width, win_height, window)
     font.init()
     font1 = font.Font(None, 35)
-    lose1 = font1.render('PLAYER 1 LOSE!', True, (180, 0, 0))
-    lose2 = font1.render('PLAYER 2 LOSE!', True, (180, 0, 0))
-    speed_x = 3
-    speed_y = 3
+    lose1 = font1.render('BLUE WINS!', True, (0, 190, 255))
+    lose2 = font1.render('YELLOW WINS!', True, (255, 255, 0))
+    #text_time = font1.render("Длительность игры: " + str(time.time() - start_time), True, (255, 255, 255))
+    speed_x = 5
+    speed_y = 5
     while run:
         for e in event.get():
             if e.type == QUIT:
                 run = False
         if finish != True:
-            window.fill(back)
+            window.blit(background, (0, 0))
+            #window.blit(text_time, 500, 50)
             racket1.reset()       
             racket2.reset()
             ball.reset()
@@ -40,10 +46,10 @@ def game():
             racket1.update_l()
             if ball.rect.x < 0:
                 finish = True
-                window.blit(lose1, (200, 200))
+                window.blit(lose1, (220, 150))
             if ball.rect.x > win_width:
                 finish = True
-                window.blit(lose2, (200, 200))
+                window.blit(lose2, (220, 150))
         display.update()
         clock.tick(FPS)
 
