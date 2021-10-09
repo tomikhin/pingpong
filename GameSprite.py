90166107861a5f47f56e3044298e88fa436850bb
tomikhin <90508484+tomikhin@users.noreply.github.com>
2021-10-09 10:47:53 +0300

    Update GameSprite.py

--- GameSprite.py
+++ GameSprite.py
@@ -14,7 +14,26 @@ class GameSprite(sprite.Sprite):
         #каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан 
         self.rect = self.image.get_rect() 
         self.rect.x = player_x 
-        self.rect.y = player_y 
+        self.rect.y = player_y
+        self.size_x = size_x
+        self.size_y = size_y
+        
 #метод, отрисовывающий героя на окне 
     def reset(self): 
         self.window.blit(self.image, (self.rect.x, self.rect.y)) 
+    def suit_load_l(self):
+        self.suit = 0
+        self.suits = []
+        for i in range(1,8):
+            self.suits.append(transform.scale(image.load('pixil-frame-' + str(i) + '.png'), (self.size_x, self.size_y)))
+    def suit_load_r(self):
+        self.suit = 0
+        self.suits = []
+        for i in range(7,0,-1):
+            self.suits.append(transform.scale(image.load('pixil-frame-' + str(i) + '.png'), (self.size_x, self.size_y)))
+    def suit_change(self):
+        self.image = self.suits[self.suit]
+        if self.suit < len(self.suits) - 1:
+            self.suit += 1
+        else:
+            self.suit = 0
